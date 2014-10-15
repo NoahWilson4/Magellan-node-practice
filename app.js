f7988ff7f33d0c221aed9ad5dcdf35d80e402677
NoahWilson4 <noahrwilson4@gmail.com>
2014-10-15 14:57:43 -0600

    finished

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var indexController = require('./controllers/index.js');
+var indexController = require('./controllers/index.js');
 
 var locations = require('./models/locations.js');
 
@@ -7,20 +7,11 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res){
-	console.log('test: ', locations.getPageData(0));
-	res.render('index', locations.getPageData(0));
+app.get('/', indexController.default);
 
-});
+app.get('/next', indexController.getNext)
 
-app.get('/:locationIndex', function(req, res){
-	var index = parseInt(req.params.locationIndex);
-	if (index > locations.names.length || index < 0 || isNaN(index)) {
-		res.render('dne');
-	} else {
-		res.render('index', locations.getPageData(index));
-	}
-})
+app.get('/:target', indexController.byIndex);
 
 var server = app.listen(7487, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,31 @@
+var locations = require('../models/locations.js');
+
 var indexController = {
-	index: function(req, res) {
-		res.render('index');
+	byIndex: function(req, res) {
+		var index = parseInt(req.params.target);
+		if (isNaN(index)) {
+			index = locations.names.indexOf(req.params.target);
+			if(index === -1) {
+				res.render('dne');
+			} else {
+				res.redirect('/' + index)
+			}
+		} else if (index > locations.names.length || index < 0 ){
+			res.render('dne');
+		} else	{
+			res.render('index', locations.getPageData(index));
+		}
+	},
+	getNext: function(req, res) {
+		var outputObject = {};
+		console.log('re.params test: ', req.query.location);
+		outputObject.location = req.query.location;
+		var locationIndex = locations.names.indexOf(outputObject.location);
+		outputObject.nextLocation = locationIndex === -1 ? null : locations.names[(locationIndex + 1) % locations.names.length];
+		res.send(JSON.stringify(outputObject));
+	},
+	default: function(req, res) {
+		res.redirect('/' + 0);
 	}
 };
 
